b5f6b2370c0c6505bae133e4b0e3cd9ea4b45625
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2025-06-22 23:47:47 +0200

    add support for mysql database.
    use django extensions for reset_db

--- openpetra/settings.py
+++ openpetra/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'openpetra',
     'apps.data',
 ]
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from openpetra.settings_local import *
